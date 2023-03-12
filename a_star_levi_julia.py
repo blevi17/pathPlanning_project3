eef03899afb1b8289d3a1f8d326389987086b839
blevi17 <blevi17@vt.edu>
2023-03-12 16:33:54 -0400

    Update a_star_levi_julia.py
    After slightly modifying my second function, I added the Dijkstra section that matches the pseudo code in project 2.  I changed some variable names to make it more readable for others.  Also, I added a bunch of comments as new functions need to be written to replace older ones, or at least modified from our previous project.

--- a_star_levi_julia.py
+++ a_star_levi_julia.py
@@ -63,9 +63,69 @@ def action_m(pos_act, L_act, act):
 def mat_expl(node_exp):
     x_exp = round(2 * node_exp[0])
     y_exp = round(2 * node_exp[1])
-    list_exp = [x_exp, y_exp, node_exp[2]]
 
-    return list_exp
+    return x_exp, y_exp, node_exp[2]
          
          
 ############################################ Main code ###############################################
+
+# I liked Julia's function for prompting the user for inputs, could go here
+
+# I used Priority queue (I am surpised you did not use that or heap)
+# Initialize priority q
+open_l = PriorityQueue()
+closed_l = PriorityQueue()
+q3 = PriorityQueue()  # this is just for the while loop
+uu = q3.empty()
+
+# Create the first element in the list
+node_i = []  # This needs to be changed to the customer input
+# [Cost, index, parent, [x, y, theta]]
+el1 = [0, 0, 0, node_i]
+
+# check if the initial node or goal nodes are in obstacle space
+
+
+# Starting the search
+res_g = 0  # becomes 1 or something else when we reach the goal
+id = el1[1]
+while open_l.empty() != uu and res_g == 0:
+     # pull out a node and add it to the closed list
+     x = open_l.get()
+     closed_l.put(x)
+     # pull out useful elements
+     cur_cost = x[0]
+     cur_ind = x[1]
+     cur_par = x[2]
+     cur_pos = x[4]
+
+     # check if we have reached the goal
+     if cur_pos == node_g:  # Need to add the goal node input
+          # run the backtrack function
+          #node_path = trace_back(close_l, cur_par, cur_ind)
+          print("success")
+          res_g = 1
+     # perform all possible movements
+     else:
+          for i in range(0, 5):
+               mat_exp = np.zeros(1200, 400)
+               new_pos = action_m(cur_pos, L, i)  # L is the user input for the step size of the robot
+               # Check if the new location is in the closed list or obstacle space
+               #check_cl = p2_repeat_cl(q2, new_l)
+               #check_ob = p2_coll(new_l)
+               # need to add lxu = cost2come + cost2go
+               if check_cl == 0 and check_ob == 1:
+                    check_ol = p2_repeat_cl(open_l, new_pos)
+                    if check_ol == 0:
+                        id = id + 1
+                        # lxu is cost to come plus cost to go
+                        new_node = [cur_cost + lxu, id, cur_ind, new_pos]
+                        open_l.put(new_node)
+               elif check_cl == 1 and check_ob == 1:
+                    m_co = p2_repeat_op(closed_l, new_pos)  # this is to get the cost of the repeat, needs to be remade
+                    lenj = closed_l.qsize()
+                    if m_co > (cur_cost + lxu):
+                        for j1 in range(0, lenj):
+                            if closed_l.queue[j1][4] == new_pos:
+                                m_i = closed_l.queue[j1][1]
+                                closed_l.queue[j1] = [cur_cost + lxu, m_i, cur_ind, new_pos]
